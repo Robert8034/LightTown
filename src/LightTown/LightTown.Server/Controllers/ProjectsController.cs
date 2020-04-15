706b721e0301a884f8a330054f8c5015bf027f64
Robert8034 <robert-dams@outlook.com>
2020-04-15 15:25:57 +0200

    Added Projects controller

---
+++ src/LightTown/LightTown.Server/Controllers/ProjectsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LightTown.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProjectsController : ControllerBase
+    {
+        public ProjectsController()
+        {
+
+        }
+
+        [HttpGet]
+        [Route("/{projectId}/members")]
+        public async Task<ApiResult> GetMembers(int projectId)
+        {
+            return ApiResult.NoContent();
+        }
+
+        [HttpPut]
+        [Route("/{projectId}/members/{userId}")]
+        public async Task<ApiResult> GetMember(int projectId, int userId)
+        {
+            return ApiResult.NoContent();
+        }
+    }
+}
