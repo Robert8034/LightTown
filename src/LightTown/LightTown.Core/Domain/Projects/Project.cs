247e9652f88e98c83e3be319be57d4376d95dbea
Aerts,Ramon R.A.R.F <ramon.aerts@student.fontys.nl>
2020-04-21 10:58:06 +0200

    added project models

---
+++ src/LightTown/LightTown.Core/Domain/Projects/Project.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LightTown.Core.Data;
+using LightTown.Core.Domain.Users;
+
+namespace LightTown.Core.Domain.Projects
+{
+    public class Project : BaseEntity
+    {
+        public string ProjectName { get; set; }
+        public string ProjectDescription { get; set; }
+        //public List<Post> ProjectPosts { get; set; }
+        public List<User> Members { get; set; }
+        //public List<Tag> Tags { get; set; }
+        public DateTime CreationDateTime { get; set; }
+        public DateTime LastModifiedDateTime { get; set; }
+    }
+}
