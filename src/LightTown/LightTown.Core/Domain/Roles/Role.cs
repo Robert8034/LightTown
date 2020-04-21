3d86f0aadb7d3a731e1037adb28bc2c8fa7c80ed
giepske <17519231+giepske@users.noreply.github.com>
2020-04-21 12:13:56 +0200

    Default data (roles and admin user) now gets created when no tables exist.

--- src/LightTown/LightTown.Core/Domain/Roles/Role.cs
+++ src/LightTown/LightTown.Core/Domain/Roles/Role.cs
@@ -4,10 +4,12 @@ namespace LightTown.Core.Domain.Roles
 {
     public class Role : IdentityRole<int>
     {
+        public bool CanBeModified { get; set; }
         public Permissions Permissions { get; set; }
 
-        public Role(string name, Permissions permissions)
+        public Role(string name, Permissions permissions, bool canBeModified = true)
         {
+            CanBeModified = canBeModified;
             base.Name = name;
             Permissions = permissions;
         }
--- src/LightTown/LightTown.Server.Data/ApplicationBuilderExtensions.cs
+++ src/LightTown/LightTown.Server.Data/ApplicationBuilderExtensions.cs
@@ -2,24 +2,42 @@
 using LightTown.Core.Domain.Users;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace LightTown.Server.Data
 {
     public static class ApplicationBuilderExtensions
     {
+        /// <summary>
+        /// Ensure that the database exists with all its tables and default data.
+        /// </summary>
+        /// <param name="applicationBuilder"></param>
         public static void EnsureMigrated(this IApplicationBuilder applicationBuilder)
         {
             using (var scope = applicationBuilder.ApplicationServices.CreateScope())
             {
                 var dbContext = scope.ServiceProvider.GetService<LightTownServerContext>();
 
+                var relationalDatabaseCreator =
+                    (RelationalDatabaseCreator) dbContext.Database.GetService<IDatabaseCreator>();
+                var tablesExists = relationalDatabaseCreator.HasTables();
+                
                 dbContext.Database.Migrate();
 
+                if (!tablesExists)
+                {
+                    RoleManager<Role> roleManager = scope.ServiceProvider.GetService<RoleManager<Role>>();
+                    roleManager.CreateAsync(new Role("Administrator", Permissions.ALL, false));
+                    roleManager.CreateAsync(new Role("Manager", Permissions.ALL));
+                    roleManager.CreateAsync(new Role("Employee", Permissions.ALL));
+
+                    UserManager<User> userManager = scope.ServiceProvider.GetService<UserManager<User>>();
+                    userManager.CreateAsync(new User("admin"), "admin");
+                }
             }
         }
-
     }
 }
