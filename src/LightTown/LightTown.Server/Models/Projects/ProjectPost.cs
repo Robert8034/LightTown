9e7e5705e9670458ab2486401d1a4ff1ceba0145
Robert8034 <robert-dams@outlook.com>
2020-04-21 15:22:01 +0200

    Added front end for creation of new projects, changed front end for list of exsisting projects and added new method to http extension & validationService

---
+++ src/LightTown/LightTown.Server/Models/Projects/ProjectPost.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LightTown.Server.Models.Projects
+{
+    public class ProjectPost
+    {
+        public string ProjectName { get; set; }
+
+        public string Description { get; set; }
+    }
+}
