8a10e2941d77af94b17b22454a50fb0da460742e
Aerts,Ramon R.A.R.F <ramon.aerts@student.fontys.nl>
2020-04-22 15:06:54 +0200

    migrations

--- src/LightTown/LightTown.Server.Data/Migrations/20200422130423_InitialCreate.Designer.cs
+++ src/LightTown/LightTown.Server.Data/Migrations/20200422130423_InitialCreate.Designer.cs
@@ -10,7 +10,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace LightTown.Server.Data.Migrations
 {
     [DbContext(typeof(LightTownServerContext))]
-    [Migration("20200422114103_InitialCreate")]
+    [Migration("20200422130423_InitialCreate")]
     partial class InitialCreate
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
