eb10db3ecd787b808227be2ea7a51018dd58f441
giepske <17519231+giepske@users.noreply.github.com>
2020-05-06 17:33:47 +0200

    Added unit tests for UserSessionService which covers login flow.

---
+++ src/LightTown/LightTown.Client.Tests/MockExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using Moq;
+using Moq.Protected;
+using Newtonsoft.Json;
+
+namespace LightTown.Client.Tests
+{
+    public static class MockExtensions
+    {
+        /// <summary>
+        /// Setup the HttpMessageHandler Mock with expected return data.
+        /// </summary>
+        /// <param name="httpMessageHandlerMock"></param>
+        /// <param name="httpStatusCode">The expected status code.</param>
+        /// <param name="content">The expected content which will be serialized to JSON.</param>
+        public static void SetupHttpMessageHandlerMock(this Mock<HttpMessageHandler> httpMessageHandlerMock, HttpStatusCode httpStatusCode, object content)
+        {
+            httpMessageHandlerMock
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>()
+                )
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = httpStatusCode,
+                    Content = new StringContent(JsonConvert.SerializeObject(content))
+                })
+                .Verifiable();
+        }
+
+        /// <summary>
+        /// Setup the IJSRuntime Mock with method name and expected return data.
+        /// </summary>
+        /// <param name="ijsRuntimeMock"></param>
+        /// <param name="identifier">The method name to call.</param>
+        /// <param name="returnData">The data to return.</param>
+        /// <param name="args">The optional arguments for the method.</param>
+        public static void SetupIjsRuntimeMock<T>(this Mock<IJSRuntime> ijsRuntimeMock, string identifier, T returnData, params object[] args)
+        {
+            ijsRuntimeMock
+                .Setup(runtime => runtime.InvokeAsync<T>(identifier, args))
+                .ReturnsAsync(returnData)
+                .Verifiable();
+        }
+    }
+}
---
+++ src/LightTown/LightTown.Client.Tests/Services/Users/UserSessionServiceTests.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+using LightTown.Client.Services.Users;
+using LightTown.Core;
+using LightTown.Core.Models.Users;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using Moq;
+using Moq.Protected;
+using Newtonsoft.Json;
+using Xunit;
+using Xunit.Sdk;
+
+namespace LightTown.Client.Tests.Services.Users
+{
+    public class UserSessionServiceTests
+    {
+        private readonly Mock<HttpMessageHandler> _httpMessageHandlerMock;
+        private readonly HttpClient _httpClient;
+        private Mock<IJSRuntime> _ijsRuntimeMock;
+        private Mock<MockNavigationManager> _navigationManagerMock;
+
+        public UserSessionServiceTests()
+        {
+            _httpMessageHandlerMock = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+            
+            _httpClient = new HttpClient(_httpMessageHandlerMock.Object)
+            {
+                BaseAddress = new Uri("https://localhost:5001/")
+            };
+
+            _ijsRuntimeMock = new Mock<IJSRuntime>(MockBehavior.Strict);
+
+            _navigationManagerMock = new Mock<MockNavigationManager>( MockBehavior.Loose);
+        }
+
+        [Fact]
+        public async Task LoadUserTest()
+        {
+            //ARRANGE
+            _httpMessageHandlerMock.SetupHttpMessageHandlerMock(HttpStatusCode.OK, new ApiResult(HttpStatusCode.OK, new User
+            {
+                Username = "TestUser"
+            }));
+
+            var userSessionService = new UserSessionService(_httpClient, null, null);
+
+            //ACT
+            await userSessionService.LoadUser();
+
+            //ASSERT
+            _httpMessageHandlerMock.Protected().Verify("SendAsync", Times.Exactly(1),
+                ItExpr.Is<HttpRequestMessage>(req =>
+                    req.Method == HttpMethod.Get
+                    && req.RequestUri == new Uri("https://localhost:5001/api/users/@me")
+                ),
+                ItExpr.IsAny<CancellationToken>()
+            );
+
+            Assert.Equal("TestUser", userSessionService.GetCurrentUser().Username);
+        }
+
+        [Fact]
+        public void SetCurrentUserTest()
+        {
+            //ARRANGE
+            var userSessionService = new UserSessionService(_httpClient, null, null);
+
+            //ACT
+            userSessionService.SetCurrentUser(new User
+            {
+                Username = "TestUser"
+            });
+
+            //ASSERT
+            Assert.NotNull(userSessionService.GetCurrentUser());
+            Assert.Equal("TestUser", userSessionService.GetCurrentUser().Username);
+        }
+        
+        /// <summary>
+        /// Test whether TryLoadLocalUser returns the right value based on the cookie value it should get from the client.
+        /// </summary>
+        /// <param name="cookie">The cookie to test with.</param>
+        /// <param name="isValid">If the cookie is expected to be valid.</param>
+        /// <returns></returns>
+        [Theory]
+        [InlineData(null, false)]
+        [InlineData("", false)]
+        [InlineData("test", false)]
+        [InlineData(".AspNetCore.Identity.Application=CfDJ8Dxi8SoNHMxCgS_gub9OejXxSkrDzPalNO-B0f23jNqIMP_FNBBj2bptK4MnST2WQ6dAG6GaWeP12uCPp6SPez2JgKx5S0cPLARg7eXyTousoxuk65IijlEy_-mXbtZOCmXxsF1sHIUjxQiZG2-HcE45485agdKuY2nFJltQwXQ-49YL4Uqg2NuAutdZf_UVlKMfRWUe-Dn_78_JmzqynjTa7dyDnIZAp7l8D5LIvCmxlb2M2ukXKTq6Jphgs5nnbgHJhJRIUiH9CFaC8enDD9lVCKu65kWOxCIttbBEYxuD7lae7n7z1gEumAO3Cg0yxnDV7YB9OiZFXUeJ-fWNbnQb1PkxQQ4Zle4PuzdI0UFJD8JjMQYaPLJnwmNAzbO2ks2bIsRXpD9Ge-yzS0odNZPjPgQHtMcApMfzlOVCgOGyG068kP3cKX1RTfjvb6S-6Te_sbVtsOBihjA9sFmMNb1BN9l9pD1CsbVEH8P_VJorlZqcxIPJpNNycf4GPP75uGkje5rH21F0YDeSZS_HHHDCF_fvMUWVbaedvGnMZdp6yhYFWhfh1QePAvpdr-uZ2LO6hPiDG5Wbo0gNl-NmpJ0qxKUYAWyxXtbOdhm00gWtaSpcBJF-jDpeO52ZAUUG_VAYNH_kps7Pam4B7MrkiShJJRHn7TXFCs_xb69PW7jihHtBccYU-w2V1CWTAiM4tg", true)]
+        public async Task TryLoadLocalUserTest(string cookie, bool isValid)
+        {
+            //ARRANGE
+            _ijsRuntimeMock.SetupIjsRuntimeMock<string>("getCookies", cookie);
+
+            var userSessionService = new UserSessionService(_httpClient, _ijsRuntimeMock.Object, _navigationManagerMock.Object);
+
+            //ACT
+            bool result = await userSessionService.TryLoadLocalUser();
+
+            //ASSERT
+            Assert.Equal(isValid, result);
+        }
+    }
+}
