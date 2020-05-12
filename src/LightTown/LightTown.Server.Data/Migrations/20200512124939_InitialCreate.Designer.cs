0fc4f487d646485479192a9a7d5cbe36dc6c5b50
giepske <17519231+giepske@users.noreply.github.com>
2020-05-12 15:05:07 +0200

    Fixed project page error

--- src/LightTown/LightTown.Server.Data/Migrations/20200512124939_InitialCreate.Designer.cs
+++ src/LightTown/LightTown.Server.Data/Migrations/20200512124939_InitialCreate.Designer.cs
@@ -10,7 +10,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace LightTown.Server.Data.Migrations
 {
     [DbContext(typeof(LightTownServerContext))]
-    [Migration("20200512101239_InitialCreate")]
+    [Migration("20200512124939_InitialCreate")]
     partial class InitialCreate
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
