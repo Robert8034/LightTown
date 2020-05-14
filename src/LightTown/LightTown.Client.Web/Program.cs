2cc93b86515ff083d8179b6600ad24acccf16251
giepske <17519231+giepske@users.noreply.github.com>
2020-05-14 22:01:14 +0200

    Added alert system for clients + buttons are now enabled by default (bug)

--- src/LightTown/LightTown.Client.Web/Program.cs
+++ src/LightTown/LightTown.Client.Web/Program.cs
@@ -1,10 +1,13 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Blazored.LocalStorage;
+using LightTown.Client.Services.Abstraction;
 using LightTown.Client.Services.Authentication;
 using LightTown.Client.Services.Projects;
 using LightTown.Client.Services.Users;
 using LightTown.Client.Services.Validation;
+using LightTown.Client.Web.Services;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -30,6 +33,7 @@ namespace LightTown.Client.Web
             builder.Services.AddSingleton<IProjectService, ProjectService>();
             builder.Services.AddSingleton<IUserAuthService, UserAuthService>();
             builder.Services.AddSingleton<IUserDataService, UserDataService>();
+            builder.Services.AddSingleton<IAlertService<AlertService.Alert>, AlertService>();
             builder.Services.AddBaseAddressHttpClient();
 
             var host = builder.Build();
---
+++ src/LightTown/LightTown.Client/Services/Abstraction/IAlertService.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LightTown.Client.Services.Abstraction
+{
+    /// <summary>
+    /// Alert service that should be implemented in every client.
+    /// </summary>
+    public interface IAlertService<T>
+    {
+        /// <summary>
+        /// The list of alerts, add your current alerts here and remove them when they get closed.
+        /// </summary>
+        Dictionary<T, AlertType> Alerts { get; set; }
+
+        /// <summary>
+        /// This should be invoked whenever a "Show...Alert()" method gets called by the implementation of this <see cref="IAlertService{T}"/>. Set this action on your client so it can update UI when it gets called.
+        /// </summary>
+        Action<T> OnShowAlert { get; set; }
+
+        /// <summary>
+        /// This should be invoked whenever a Alert gets removed from the <see cref="Alerts"/> list. Set this action on your client so it can update UI when it gets called.
+        /// </summary>
+        Action<T> OnCloseAlert { get; set; }
+
+        /// <summary>
+        /// Show an error alert to the user.
+        /// </summary>
+        /// <param name="closable">Whether the alert should be closable or not.</param>
+        /// <param name="closesAfter">How long it takes before the alert closes by itself, <see langword="null"></see> to not close it automatically.</param>
+        /// <param name="body">The body text for the alert.</param>
+        /// <param name="title">The title for the alert, optional.</param>
+        void ShowErrorAlert(bool closable, TimeSpan? closesAfter, string body, string title = null);
+
+        /// <summary>
+        /// Show a warning alert to the user.
+        /// </summary>
+        /// <param name="closable">Whether the alert should be closable or not.</param>
+        /// <param name="closesAfter">How long it takes before the alert closes by itself, <see langword="null"></see> to not close it automatically.</param>
+        /// <param name="body">The body text for the alert.</param>
+        /// <param name="title">The title for the alert, optional.</param>
+        void ShowWarningAlert(bool closable, TimeSpan? closesAfter, string body, string title = null);
+
+        /// <summary>
+        /// Show a standard alert to the user.
+        /// </summary>
+        /// <param name="closable">Whether the alert should be closable or not.</param>
+        /// <param name="closesAfter">How long it takes before the alert closes by itself, <see langword="null"></see> to not close it automatically.</param>
+        /// <param name="body">The body text for the alert.</param>
+        /// <param name="title">The title for the alert, optional.</param>
+        void ShowStandardAlert(bool closable, TimeSpan? closesAfter, string body, string title = null);
+
+        /// <summary>
+        /// Show a success alert to the user.
+        /// </summary>
+        /// <param name="closable">Whether the alert should be closable or not.</param>
+        /// <param name="closesAfter">How long it takes before the alert closes by itself, <see langword="null"></see> to not close it automatically.</param>
+        /// <param name="body">The body text for the alert.</param>
+        /// <param name="title">The title for the alert, optional.</param>
+        void ShowSuccessAlert(bool closable, TimeSpan? closesAfter, string body, string title = null);
+    }
+
+    public enum AlertType
+    {
+        Error,
+        Warning,
+        Standard,
+        Success
+    }
+}
