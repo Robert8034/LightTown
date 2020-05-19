cb4682083b9103235f409d90005708f398bd8aaf
Robert8034 <robert-dams@outlook.com>
2020-05-19 14:19:16 +0200

    Changes: - Added more CSS to ProjectPage

--- src/LightTown/LightTown.Server.Data/Migrations/20200519115228_InitialCreate.Designer.cs
+++ src/LightTown/LightTown.Server.Data/Migrations/20200519115228_InitialCreate.Designer.cs
@@ -10,7 +10,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace LightTown.Server.Data.Migrations
 {
     [DbContext(typeof(LightTownContext))]
-    [Migration("20200519094734_InitialCreate")]
+    [Migration("20200519115228_InitialCreate")]
     partial class InitialCreate
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
